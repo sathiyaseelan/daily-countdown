abe697fe64fb1e68f3d1c332a5752bb39685a327
sathiyaseelan <ssstarlet@gmail.com>
2016-05-26 20:36:33 +0000

    Added Bing image api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,25 @@
+require 'rest-client'
 class PagesController < ApplicationController
     
+    
     def home
+        @bg_image_url = get_image_url
     end
 
+
+    private
+    
+    def get_image_url
+        response = RestClient.get 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=12'
+        if response.code == 200
+            response_json = JSON.parse(response.body)
+            puts response_json
+            # 'http://www.bing.com/az/hprichbg/rb/Paraglider_EN-US7985350118_1920x1080.jpg'
+            random = Random.new
+            img_index = random.rand(0...8)
+            return "http://www.bing.com" << response_json["images"][img_index]["url"]
+        else
+             return ActionController::Base.helpers.asset_path('timer_bg.jpg')
+        end
+    end
 end
